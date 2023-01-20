56ec78ad1bf65a745f1c4b6e87118106d956bdb2
Muhammed Sahin <121553425+azulawashere@users.noreply.github.com>
2023-01-20 21:00:53 +0300

    Added project

---
+++ Akinsof_cloe/Admin.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Akinsof_cloe
+{
+    partial class Admin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_hour = new System.Windows.Forms.MaskedTextBox();
+            this.txt_min = new System.Windows.Forms.MaskedTextBox();
+            this.txt_sec = new System.Windows.Forms.MaskedTextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(216, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Timer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(11, 114);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(302, 23);
+            this.progressBar1.TabIndex = 1;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_sec);
+            this.groupBox1.Controls.Add(this.txt_min);
+            this.groupBox1.Controls.Add(this.txt_hour);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 11);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(301, 97);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Timer Set";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hour:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(95, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Min:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(194, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Sec:";
+            // 
+            // txt_hour
+            // 
+            this.txt_hour.Location = new System.Drawing.Point(42, 39);
+            this.txt_hour.Mask = "00";
+            this.txt_hour.Name = "txt_hour";
+            this.txt_hour.Size = new System.Drawing.Size(35, 20);
+            this.txt_hour.TabIndex = 2;
+            this.txt_hour.ValidatingType = typeof(int);
+            // 
+            // txt_min
+            // 
+            this.txt_min.Location = new System.Drawing.Point(138, 39);
+            this.txt_min.Mask = "00";
+            this.txt_min.Name = "txt_min";
+            this.txt_min.Size = new System.Drawing.Size(35, 20);
+            this.txt_min.TabIndex = 2;
+            this.txt_min.ValidatingType = typeof(int);
+            // 
+            // txt_sec
+            // 
+            this.txt_sec.Location = new System.Drawing.Point(229, 39);
+            this.txt_sec.Mask = "00";
+            this.txt_sec.Name = "txt_sec";
+            this.txt_sec.Size = new System.Drawing.Size(38, 20);
+            this.txt_sec.TabIndex = 2;
+            this.txt_sec.ValidatingType = typeof(int);
+            // 
+            // Admin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 175);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Admin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Focus Mod";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox txt_sec;
+        private System.Windows.Forms.MaskedTextBox txt_min;
+        private System.Windows.Forms.MaskedTextBox txt_hour;
+    }
+}
---
+++ Akinsof_cloe/Admin.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Akinsof_cloe
+{
+    public partial class Admin : Form
+    {
+        public Admin()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   
+            int hour = Convert.ToInt32(txt_hour.Text);
+            int min = Convert.ToInt32(txt_min.Text) + (hour * 60) ;
+            int sec = Convert.ToInt32(txt_sec.Text) + (min * 60) ;
+            progressBar1.Maximum= sec;
+            progressBar1.Value = sec;
+            timer1.Start();
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+        SoundPlayer player;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Value -= 1;
+            if (progressBar1.Value==0)
+            {
+                timer1.Stop();
+                SoundPlayer ses = new SoundPlayer();
+                ses.SoundLocation = "sound.wav";
+                ses.Play();
+                MessageBox.Show("Time is END","ALERT",MessageBoxButtons.OK);
+            }
+        }
+    }
+}
